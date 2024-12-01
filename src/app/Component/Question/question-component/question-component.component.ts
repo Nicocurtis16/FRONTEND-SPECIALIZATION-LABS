c1756e32a015124698010c90c9767ef3eacef7b0
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-01 15:30:52 +0000

    the options with the submit button display

--- src/app/Component/Question/question-component/question-component.component.ts
+++ src/app/Component/Question/question-component/question-component.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {OptionsComponentComponent} from "../../Options/options-component/options-component.component";
+import { OptionsComponentComponent } from "../../Options/options-component/options-component.component";
 
 @Component({
   selector: 'app-question-component',
@@ -10,9 +10,31 @@ import {OptionsComponentComponent} from "../../Options/options-component/options
   styleUrls: ['./question-component.component.css']
 })
 export class QuestionComponentComponent {
-  @Input() questionData: any; // Accept data from parent component
+  @Input() questionData: any;
+  currentQuestionIndex: number = 0;
+  showNextButton: boolean = false;
 
   ngOnInit() {
-    console.log('Received data:', this.questionData); // Check the received data
+    console.log('Received data:', this.questionData);
+  }
+
+  handleAnswerSubmitted(isCorrect: boolean) {
+    this.showNextButton = true;
+    // Optionally, you can add logic to track score or show a message
+  }
+
+  moveToNextQuestion() {
+    this.showNextButton = false;
+
+    // Check if there are more questions
+    if (this.questionData && this.questionData.questions &&
+      this.currentQuestionIndex + 1 < this.questionData.questions.length) {
+      this.currentQuestionIndex += 1;
+      // Update the current question
+      this.questionData = this.questionData.questions[this.currentQuestionIndex];
+    } else {
+      console.log('Quiz completed');
+      // Optionally, implement quiz completion logic
+    }
   }
 }
