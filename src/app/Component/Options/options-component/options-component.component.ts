7e559148868626679857883065efe54a2a26775d
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-02 10:23:29 +0000

    the border colors for the correct and in correct works but the img doesnt work

--- src/app/Component/Options/options-component/options-component.component.ts
+++ src/app/Component/Options/options-component/options-component.component.ts
@@ -12,17 +12,21 @@ import { FormsModule } from '@angular/forms';
 export class OptionsComponentComponent {
   @Input() options: string[] = [];
   @Input() correctAnswer: string = '';
+  @Input() correctIcon: string = 'src/assets/images/correct.svg';
+  @Input() incorrectIcon: string = 'src/assets/images/wrong.svg';
+
   @Output() answerSubmitted = new EventEmitter<boolean>();
   @Output() nextQuestionEvent = new EventEmitter<void>();
-  hoveredIndex: number | null = null;
+
   selectedOption: string = '';
   isSubmitted: boolean = false;
-  optionStyles: { [key: string]: string } = {};
+
+  // Track styles for each option
+  optionState: { [key: string]: string } = {};
 
   selectOption(option: string) {
     if (!this.isSubmitted) {
       this.selectedOption = option;
-      this.resetOptionStyles();
     }
   }
 
@@ -31,11 +35,17 @@ export class OptionsComponentComponent {
       this.isSubmitted = true;
       const isCorrect = this.selectedOption === this.correctAnswer;
 
+      // Reset all option states
+      this.optionState = {};
+
       if (isCorrect) {
-        this.optionStyles[this.selectedOption] = 'border-green-500';
+        // If correct, set selected option to correct
+        this.optionState[this.selectedOption] = 'correct';
       } else {
-        this.optionStyles[this.selectedOption] = 'border-red-500';
-        this.optionStyles[this.correctAnswer] = 'border-green-500';
+        // If incorrect, set selected option to incorrect
+        // and correct answer to correct
+        this.optionState[this.selectedOption] = 'incorrect';
+        this.optionState[this.correctAnswer] = 'correct';
       }
 
       this.answerSubmitted.emit(isCorrect);
@@ -50,14 +60,54 @@ export class OptionsComponentComponent {
   private resetForNextQuestion() {
     this.selectedOption = '';
     this.isSubmitted = false;
-    this.resetOptionStyles();
-  }
-
-  private resetOptionStyles() {
-    this.optionStyles = {};
+    this.optionState = {};
   }
 
   getAlphabet(index: number): string {
     return String.fromCharCode(65 + index);
+  }
+
+  // Method to determine label border class
+  getLabelBorderClass(option: string): string {
+    if (!this.isSubmitted) return '';
+
+    switch(this.optionState[option]) {
+      case 'correct': return 'border-green-500';
+      case 'incorrect': return 'border-red-500';
+      default: return '';
+    }
+  }
+
+  // Method to determine index panel class
+  getIndexPanelClass(option: string): string {
+    if (!this.isSubmitted) return '';
+
+    switch(this.optionState[option]) {
+      case 'correct': return 'correct-panel';
+      case 'incorrect': return 'incorrect-panel';
+      default: return '';
+    }
+  }
+
+  // Method to determine index text class
+  getIndexTextClass(option: string): string {
+    if (!this.isSubmitted) return '';
+
+    switch(this.optionState[option]) {
+      case 'correct': return 'correct-index';
+      case 'incorrect': return 'incorrect-index';
+      default: return '';
+    }
+  }
+
+  // Method to get icon for option
+  getOptionIcon(option: string): string {
+    if (!this.isSubmitted) return '';
+
+    switch(this.optionState[option]) {
+      case 'correct': return 'src/assets/images/correct.svg';
+      case 'incorrect': return 'src/assets/images/wrong.svg';
+      default: return '';
+    }
   }
 }
