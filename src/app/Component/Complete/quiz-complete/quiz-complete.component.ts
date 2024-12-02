e5d7d4b95d44ff7c1d2c730c7d3a259fe7892fe7
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-02 16:24:23 +0000

    the title and icon is been displayed on the quizcomplete component

--- src/app/Component/Complete/quiz-complete/quiz-complete.component.ts
+++ src/app/Component/Complete/quiz-complete/quiz-complete.component.ts
@@ -1,18 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component,Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-quiz-complete',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <div class="quiz-completed" style="border: 3px solid red">
-      <h3>Quiz Completed!</h3>
-      <p>You scored {{ correctAnswersCount }} out of {{ totalQuestions }}</p>
-    </div>
-  `
+  templateUrl: './quiz-complete.component.html',
+  styleUrl: './quiz-complete.component.css'
 })
 export class QuizCompleteComponent {
   @Input() correctAnswersCount: number = 0;
   @Input() totalQuestions: number = 0;
+  @Input() quizTitle: string = '';
+  @Input() quizIcon: string = '';
 }
