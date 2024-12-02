1989aaf9d6b58284beb003f3f3e58716b8b44cd5
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-02 16:08:59 +0000

    now the score displays on different component as expected

--- src/app/Component/Complete/quiz-complete/quiz-complete.component.ts
+++ src/app/Component/Complete/quiz-complete/quiz-complete.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
-import {ButtonComponentComponent} from "../../Button/button-component/button-component.component";
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quiz-complete',
   standalone: true,
-  imports: [
-    ButtonComponentComponent
-  ],
-  templateUrl: './quiz-complete.component.html',
-  styleUrl: './quiz-complete.component.css'
+  imports: [CommonModule],
+  template: `
+    <div class="quiz-completed" style="border: 3px solid red">
+      <h3>Quiz Completed!</h3>
+      <p>You scored {{ correctAnswersCount }} out of {{ totalQuestions }}</p>
+    </div>
+  `
 })
 export class QuizCompleteComponent {
-
+  @Input() correctAnswersCount: number = 0;
+  @Input() totalQuestions: number = 0;
 }
--- src/app/Component/Question/question-component/question-component.component.ts
+++ src/app/Component/Question/question-component/question-component.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OptionsComponentComponent } from "../../Options/options-component/options-component.component";
+import { QuizCompleteComponent } from '../../Complete/quiz-complete/quiz-complete.component';
 
 @Component({
   selector: 'app-question-component',
   standalone: true,
-  imports: [CommonModule, OptionsComponentComponent],
+  imports: [CommonModule, OptionsComponentComponent, QuizCompleteComponent],
   templateUrl: './question-component.component.html',
   styleUrls: ['./question-component.component.css']
 })
@@ -16,35 +17,23 @@ export class QuestionComponentComponent implements OnInit {
   totalQuestions: number = 0;
   currentQuestion: any;
   quizCompleted: boolean = false;
-
-  // New property for progress tracking
   quizProgress: number = 0;
-
-  // Existing properties
   quizTitle: string = '';
   quizIcon: string = '';
   correctAnswersCount: number = 0;
 
   ngOnInit() {
-    console.log('Full Quiz Data:', this.questionData);
-
-    if (this.questionData && this.questionData.questions) {
+    if (this.questionData?.questions) {
       this.quizTitle = this.questionData.title;
       this.quizIcon = this.questionData.icon;
-
       this.totalQuestions = this.questionData.questions.length;
       this.currentQuestion = this.questionData.questions[this.currentQuestionIndex];
-
-      // Calculate initial progress
       this.calculateQuizProgress();
     }
   }
 
-  // Calculate quiz progress
   calculateQuizProgress() {
-    if (this.totalQuestions > 0) {
-      this.quizProgress = ((this.currentQuestionIndex + 1) / this.totalQuestions) * 100;
-    }
+    this.quizProgress = ((this.currentQuestionIndex + 1) / this.totalQuestions) * 100;
   }
 
   handleAnswerSubmitted(isCorrect: boolean) {
@@ -58,8 +47,6 @@ export class QuestionComponentComponent implements OnInit {
 
     if (this.currentQuestionIndex < this.totalQuestions) {
       this.currentQuestion = this.questionData.questions[this.currentQuestionIndex];
-
-      // Recalculate progress
       this.calculateQuizProgress();
     } else {
       this.quizCompleted = true;
