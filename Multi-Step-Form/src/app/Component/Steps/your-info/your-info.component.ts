92ab143cbea3187c97d47101d7cdf679ff4d27ad
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-05 15:31:36 +0000

    the yourinfo component is styled,checks the input filed and resposnivness as well

--- Multi-Step-Form/src/app/Component/Steps/your-info/your-info.component.ts
+++ Multi-Step-Form/src/app/Component/Steps/your-info/your-info.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import {FormsModule,ReactiveFormsModule,} from "@angular/forms";
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-your-info',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './your-info.component.html',
   styleUrl: './your-info.component.css'
 })
 export class YourInfoComponent {
+  yourInfoForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.yourInfoForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]]
+    });
+  }
+
+  get name() { return this.yourInfoForm.get('name') as any; }
+  get email() { return this.yourInfoForm.get('email') as any; }
+  get phone() { return this.yourInfoForm.get('phone') as any; }
 }
