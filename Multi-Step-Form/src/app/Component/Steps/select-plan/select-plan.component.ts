8b056f86853bcaeaad5bbc49012c479c797cdd20
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-06 13:23:17 +0000

    styeled the UI for the plan componet left witht he fetching of data

--- Multi-Step-Form/src/app/Component/Steps/select-plan/select-plan.component.ts
+++ Multi-Step-Form/src/app/Component/Steps/select-plan/select-plan.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-select-plan',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './select-plan.component.html',
-  styleUrl: './select-plan.component.css'
+  styleUrls: ['./select-plan.component.css']
 })
 export class SelectPlanComponent {
+  monthlyPlans = [
+    {name: 'Basic', price: '$10/month'},
+    {name: 'Standard', price: '$20/month'},
+    {name: 'Premium', price: '$30/month'}
+  ];
 
+  yearlyPlans = [
+    {name: 'Basic', price: '$100/year'},
+    {name: 'Standard', price: '$200/year'},
+    {name: 'Premium', price: '$300/year'}
+  ];
+
+  isMonthly = true;
+
+  togglePlan() {
+    this.isMonthly = !this.isMonthly;
+  }
 }
