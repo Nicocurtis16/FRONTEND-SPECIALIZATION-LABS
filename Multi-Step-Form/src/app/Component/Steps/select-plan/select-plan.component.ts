567ff0c92d063a5469d4ca32b586eaa9f40aff72
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-07 07:09:39 +0000

    styeled the UI for the plan componet left witht he fetching of data  toggle between the months and year

--- Multi-Step-Form/src/app/Component/Steps/select-plan/select-plan.component.ts
+++ Multi-Step-Form/src/app/Component/Steps/select-plan/select-plan.component.ts
@@ -1,30 +1,31 @@
 import { Component } from '@angular/core';
-import {NgForOf, NgIf} from "@angular/common";
+import {NgClass, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-select-plan',
   standalone: true,
   imports: [
     NgIf,
-    NgForOf
+    NgForOf,
+    NgClass
   ],
   templateUrl: './select-plan.component.html',
   styleUrls: ['./select-plan.component.css']
 })
 export class SelectPlanComponent {
-  monthlyPlans = [
-    {name: 'Basic', price: '$10/month'},
-    {name: 'Standard', price: '$20/month'},
-    {name: 'Premium', price: '$30/month'}
-  ];
-
   yearlyPlans = [
-    {name: 'Basic', price: '$100/year'},
-    {name: 'Standard', price: '$200/year'},
-    {name: 'Premium', price: '$300/year'}
+    { name: 'Arcade', price: '$9/mo', imgUrl: 'assets/images/icon-arcade.svg' },
+    { name: 'Advance', price: '$12/mo', imgUrl: 'assets/images/icon-advanced.svg' },
+    { name: 'Pro', price: '$15/mo', imgUrl: 'assets/images/icon-pro.svg' }
   ];
 
-  isMonthly = true;
+ monthlyPlans = [
+   { name: 'Arcade', price: '$90/mo', imgUrl: 'assets/images/icon-arcade.svg' },
+   { name: 'Advance', price: '$120/mo', imgUrl: 'assets/images/icon-advanced.svg' },
+   { name: 'Pro', price: '$150/mo', imgUrl: 'assets/images/icon-pro.svg' }
+ ];
+
+  isMonthly = false;
 
   togglePlan() {
     this.isMonthly = !this.isMonthly;
