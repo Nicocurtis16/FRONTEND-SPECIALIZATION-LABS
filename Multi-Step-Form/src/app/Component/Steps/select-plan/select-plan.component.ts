b9fbd63816e720c247a97a97aa36c412f9bbe0f7
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-09 09:08:37 +0000

    styled the desktop view for the select plan

--- Multi-Step-Form/src/app/Component/Steps/select-plan/select-plan.component.ts
+++ Multi-Step-Form/src/app/Component/Steps/select-plan/select-plan.component.ts
@@ -13,21 +13,26 @@ import {NgClass, NgForOf, NgIf} from '@angular/common';
   styleUrls: ['./select-plan.component.css']
 })
 export class SelectPlanComponent {
+  selectedPlan: any;
   yearlyPlans = [
     { name: 'Arcade', price: '$9/mo', imgUrl: 'assets/images/icon-arcade.svg' },
     { name: 'Advance', price: '$12/mo', imgUrl: 'assets/images/icon-advanced.svg' },
-    { name: 'Pro', price: '$15/mo', imgUrl: 'assets/images/icon-pro.svg' }
+    { name: 'Pro', price: '$15/mo', imgUrl: 'assets/images/icon-pro.svg'}
   ];
 
  monthlyPlans = [
-   { name: 'Arcade', price: '$90/mo', imgUrl: 'assets/images/icon-arcade.svg' },
-   { name: 'Advance', price: '$120/mo', imgUrl: 'assets/images/icon-advanced.svg' },
-   { name: 'Pro', price: '$150/mo', imgUrl: 'assets/images/icon-pro.svg' }
+   { name: 'Arcade', price: '$90/mo', imgUrl: 'assets/images/icon-arcade.svg' ,free:'2 months free' },
+   { name: 'Advance', price: '$120/mo', imgUrl: 'assets/images/icon-advanced.svg' ,free:'2 months free'},
+   { name: 'Pro', price: '$150/mo', imgUrl: 'assets/images/icon-pro.svg',free:'2 months free' }
  ];
 
   isMonthly = false;
 
   togglePlan() {
     this.isMonthly = !this.isMonthly;
+  }
+  selectPlan(plan: any) {
+    this.selectedPlan = plan;
+    console.log('Selected plan:', plan);
   }
 }
