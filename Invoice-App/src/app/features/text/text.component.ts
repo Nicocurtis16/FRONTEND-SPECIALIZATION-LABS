c8afb49cb264cc921201e60b36f20243571cbcf7
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-18 08:37:37 +0000

    set the varrient and styles for the body1 and body2 for the text component

--- Invoice-App/src/app/features/text/text.component.ts
+++ Invoice-App/src/app/features/text/text.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import {NgClass} from "@angular/common";
+
 
 @Component({
   selector: 'app-text',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './text.component.html',
   styleUrl: './text.component.css'
 })
 export class TextComponent {
-
+@Input() variant : 'body1' | 'body2' = 'body1';
+@Input() text: string = '';
 }
