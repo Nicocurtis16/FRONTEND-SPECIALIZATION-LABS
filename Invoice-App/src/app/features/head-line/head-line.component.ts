4c878ad2322e2421293f1f007e5bc77a86b808e7
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-18 08:27:28 +0000

    set the varrient and styles for the various H1 to H4 i have in the UI

--- Invoice-App/src/app/features/head-line/head-line.component.ts
+++ Invoice-App/src/app/features/head-line/head-line.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-head-line',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './head-line.component.html',
-  styleUrl: './head-line.component.css'
+  styleUrls: ['./head-line.component.css']
 })
 export class HeadLineComponent {
-
+  @Input() variant: 'h1' | 'h2' | 'h3' | 'h4' = 'h1';
+  @Input() text: string = '';
 }
