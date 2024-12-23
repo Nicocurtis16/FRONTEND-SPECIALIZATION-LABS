581a92082bad65d6aef6d312f42e685220906673
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-23 14:18:02 +0000

    invoice component set to fetch data and display

--- Invoice-App/src/app/features/layout/layout.component.ts
+++ Invoice-App/src/app/features/layout/layout.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {SidebarComponent} from "../../component/sidebar/sidebar.component";
 import {ButtonComponent} from "../button/button.component";
+import {InvoiceComponent} from "../../component/invoice/invoice.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     SidebarComponent,
-    ButtonComponent
+    ButtonComponent,
+    InvoiceComponent
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
