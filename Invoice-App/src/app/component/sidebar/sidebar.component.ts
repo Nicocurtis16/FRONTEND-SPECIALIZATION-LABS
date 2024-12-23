aad474056ed7c94b93b49bb00e4f249ca0f1532a
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-23 09:28:28 +0000

    service for the data set

--- Invoice-App/src/app/component/sidebar/sidebar.component.ts
+++ Invoice-App/src/app/component/sidebar/sidebar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ThemeToggleComponent} from "../../features/theme-toggle/theme-toggle.component";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    ThemeToggleComponent
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
