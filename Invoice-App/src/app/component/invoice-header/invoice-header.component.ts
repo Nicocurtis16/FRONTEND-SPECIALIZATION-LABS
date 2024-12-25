71b17db9380ab1d7e230e6753b4ef5bfcbcb9ae6
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-25 14:19:01 +0000

    imorted the button componnt at the invoiceHeader component

--- Invoice-App/src/app/component/invoice-header/invoice-header.component.ts
+++ Invoice-App/src/app/component/invoice-header/invoice-header.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {HeadLineComponent} from "../../features/head-line/head-line.component";
+import {TextComponent} from "../../features/text/text.component";
+import {ButtonComponent} from "../../features/button/button.component";
 
 @Component({
   selector: 'app-invoice-header',
   standalone: true,
-  imports: [],
+  imports: [
+    HeadLineComponent,
+    TextComponent,
+    ButtonComponent
+  ],
   templateUrl: './invoice-header.component.html',
   styleUrl: './invoice-header.component.css'
 })
