288ce2e767a4c7dfcb1e54edad6cdd6ab7d28700
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-29 14:38:53 +0000

    fixed the lenth of the detail view

--- Invoice-App/src/app/features/layout/layout.component.ts
+++ Invoice-App/src/app/features/layout/layout.component.ts
@@ -3,17 +3,32 @@ import {SidebarComponent} from "../../component/sidebar/sidebar.component";
 import {ButtonComponent} from "../button/button.component";
 import {InvoiceComponent} from "../../component/invoice/invoice.component";
 import {ViewInvoiceComponent} from "../../component/view-invoice/view-invoice.component";
+import {Invoice} from "../../service/invoice";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     SidebarComponent,
-    ViewInvoiceComponent
+    ViewInvoiceComponent,
+    InvoiceComponent,
+    NgIf
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  showInvoiceList = true; // Control which component to show
+  selectedInvoice: Invoice | null = null;
 
+  displayViewInvoice(invoice:   Invoice) {
+    this.selectedInvoice = invoice;
+    this.showInvoiceList = false;
+  }
+
+  goBackToInvoiceList() {
+    this.selectedInvoice = null;
+    this.showInvoiceList = true;
+  }
 }
