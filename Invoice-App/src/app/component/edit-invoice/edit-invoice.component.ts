e0f27980f64901fed8102d0e1cce33d0720df001
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-30 15:43:45 +0000

    styled the edit compoent with errors on the scroll

--- Invoice-App/src/app/component/edit-invoice/edit-invoice.component.ts
+++ Invoice-App/src/app/component/edit-invoice/edit-invoice.component.ts
@@ -3,6 +3,8 @@ import {SidebarComponent} from "../sidebar/sidebar.component";
 import {HeadLineComponent} from "../../features/head-line/head-line.component";
 import {TextComponent} from "../../features/text/text.component";
 import {PaymentTermsComponent} from "../../features/payment-terms/payment-terms.component";
+import {DatePickerComponent} from "../../features/date-picker/date-picker.component";
+import {ButtonComponent} from "../../features/button/button.component";
 
 @Component({
   selector: 'app-edit-invoice',
@@ -11,11 +13,22 @@ import {PaymentTermsComponent} from "../../features/payment-terms/payment-terms.
     SidebarComponent,
     HeadLineComponent,
     TextComponent,
-    PaymentTermsComponent
+    PaymentTermsComponent,
+    DatePickerComponent,
+    ButtonComponent
   ],
   templateUrl: './edit-invoice.component.html',
   styleUrl: './edit-invoice.component.css'
 })
 export class EditInvoiceComponent {
+  selectedDate: any;
 
+  onDateSelected(date: string) {
+    this.selectedDate = date;
+    console.log('Selected date:', this.selectedDate);
+  }
+
+  handleButtonClick(delete1: string) {
+
+  }
 }
