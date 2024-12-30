7e515eb8090956de892f1ee712c2cb435e29ab36
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-12-30 11:39:27 +0000

    payment terms component set

--- Invoice-App/src/app/component/edit-invoice/edit-invoice.component.ts
+++ Invoice-App/src/app/component/edit-invoice/edit-invoice.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {SidebarComponent} from "../sidebar/sidebar.component";
 import {HeadLineComponent} from "../../features/head-line/head-line.component";
 import {TextComponent} from "../../features/text/text.component";
+import {PaymentTermsComponent} from "../../features/payment-terms/payment-terms.component";
 
 @Component({
   selector: 'app-edit-invoice',
@@ -9,7 +10,8 @@ import {TextComponent} from "../../features/text/text.component";
   imports: [
     SidebarComponent,
     HeadLineComponent,
-    TextComponent
+    TextComponent,
+    PaymentTermsComponent
   ],
   templateUrl: './edit-invoice.component.html',
   styleUrl: './edit-invoice.component.css'
