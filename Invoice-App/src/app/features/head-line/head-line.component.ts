682a01896f9daca9d3b6de6ed1976684230d3672
Nicocurtis16 <nicholaskyereboah@gmail.com>
2025-01-06 14:09:07 +0000

    fix the display of the amount

--- Invoice-App/src/app/features/head-line/head-line.component.ts
+++ Invoice-App/src/app/features/head-line/head-line.component.ts
@@ -9,6 +9,8 @@ import { Component, Input } from '@angular/core';
 export class HeadLineComponent {
   @Input() variant: 'h1' | 'h2' | 'h3' | 'h4' = 'h1';
   @Input() text: string = '';
+  @Input() alignment: string = 'left';
+
   @Input() color: string = '--primary-color'; // Color for the whole text
 
 
