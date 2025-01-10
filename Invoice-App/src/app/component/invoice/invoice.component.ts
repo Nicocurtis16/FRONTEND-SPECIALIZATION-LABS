07d31de77f9e50e43f4a0e784900f250f950b040
Nicocurtis16 <nicholaskyereboah@gmail.com>
2025-01-10 07:57:56 +0000

    set the currency pipe for the decimal implmentation

--- Invoice-App/src/app/component/invoice/invoice.component.ts
+++ Invoice-App/src/app/component/invoice/invoice.component.ts
@@ -1,13 +1,12 @@
-import {Component, computed, effect, OnInit} from '@angular/core';
+import { Component, computed, OnInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {CurrencyPipe, NgFor, NgIf} from '@angular/common';
+import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 
-
 import { DataService } from '../../service/data.service';
 import { Invoice } from '../../service/invoice';
-import {selectAllInvoices, selectFilteredInvoices, selectIsLoadingState} from '../../state/selectors/invoice.selector';
+import { selectFilteredInvoices, selectIsLoadingState } from '../../state/selectors/invoice.selector';
 import { invoiceAction } from '../../state/actions/invoice.action';
 
 import { BadgeComponent } from '../../features/badge/badge.component';
@@ -15,7 +14,7 @@ import { HeadLineComponent } from '../../features/head-line/head-line.component'
 import { TextComponent } from '../../features/text/text.component';
 import { InvoiceHeaderComponent } from '../invoice-header/invoice-header.component';
 import { NoInvoiceComponent } from '../../features/no-invoice/no-invoice.component';
-import {IconComponent} from "../../features/icon/icon.component";
+import { IconComponent } from '../../features/icon/icon.component';
 
 @Component({
   selector: 'app-invoice',
@@ -29,16 +28,17 @@ import {IconComponent} from "../../features/icon/icon.component";
     NgIf,
     NoInvoiceComponent,
     IconComponent,
-
   ],
   standalone: true,
   templateUrl: './invoice.component.html',
   styleUrls: ['./invoice.component.css'],
+  providers: [CurrencyPipe], // Include CurrencyPipe in providers
 })
 export class InvoiceComponent implements OnInit {
   isLoading = this.store.selectSignal(selectIsLoadingState);
   invoices = this.store.selectSignal(selectFilteredInvoices); // All invoices from state
   selectedStatuses: string[] = []; // Filter criteria
+  formattedTotal: string = '';
 
   // Computed signals for filtered data and count
   displayedInvoices = computed(() => {
@@ -51,23 +51,23 @@ export class InvoiceComponent implements OnInit {
     );
   });
 
-
-
   invoiceCount = computed(() => this.displayedInvoices().length);
 
   constructor(
     private store: Store,
     private dataService: DataService,
-    private router: Router
-
-  ) {
-
-  }
+    private router: Router,
+    private currencyPipe: CurrencyPipe // Properly injected CurrencyPipe
+  ) {}
 
   ngOnInit() {
     if (!this.isLoading()) {
       this.store.dispatch(invoiceAction.loadInvoices());
     }
+  }
+
+  getFormattedTotal(total: number): string {
+    return this.currencyPipe.transform(total, 'GBP', 'symbol', '1.2-2') || '';
   }
 
   selectInvoice(invoice: Invoice) {
