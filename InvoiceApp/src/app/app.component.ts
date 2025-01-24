e103402fc11f7b7325e21a2809ea479093cb6375
Nicocurtis16 <nicholaskyereboah@gmail.com>
2025-01-24 15:20:10 +0000

    set the login componet

--- InvoiceApp/src/app/app.component.ts
+++ InvoiceApp/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LayoutComponent} from "./features/layout/layout.component";
+import {LoginComponent} from "./component/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [LayoutComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
